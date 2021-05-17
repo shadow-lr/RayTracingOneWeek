08a4bc851aeae81f4786b2db9a9c8463d1b54a18
shaodow_lr <shaodow_lr@163.com>
2021-05-17 22:40:19 +0800

    初始化cmake 环境

---
+++ RayTracingOneWeek/cmake-build-debug/CMakeFiles/RayTracingOneWeek.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RayTracingOneWeek.dir/main.cpp.obj"
+  "RayTracingOneWeek.exe"
+  "RayTracingOneWeek.exe.manifest"
+  "RayTracingOneWeek.pdb"
+  "libRayTracingOneWeek.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RayTracingOneWeek.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RayTracingOneWeek/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+
+double hit_sphere(const point3 &center, double radius, const ray &r) {
+    // 射线原点到中心点的位置
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto half_b = dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-half_b - sqrt(discriminant)) / (a);
+    }
+}
+
+color ray_color(const ray &r) {
+    // old
+//    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+//        return color(1, 0, 0);
+//    vec3 unit_direction = unit_vector(r.direction());
+//    auto t = 0.5 * (unit_direction.y() + 1.0);
+//    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+
+    // new
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main() {
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 1200;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+
+    double viewport_height = 2.0;
+    double viewport_width = aspect_ratio * viewport_height;
+    double focal_length = 1.0;
+
+    vec3 origin = point3(0, 0, 0);
+    vec3 horizontal = vec3(viewport_width, 0, 0);
+    vec3 vertical = vec3(0, viewport_height, 0);
+
+    // 此处设置z = -1
+    vec3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+    // -1.77778 -1 -1
+//    std::cout << lower_left_corner << "\n";
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+}
