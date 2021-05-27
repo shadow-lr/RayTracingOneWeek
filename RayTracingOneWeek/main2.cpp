99c1c947cee1b46de5694dd3f61c2b93a30dc87e
shadow-lr <shaodow_lr@163.com>
2021-05-27 21:26:24 +0800

    Finish chapter 12 content

--- RayTracingOneWeek/main2.cpp
+++ RayTracingOneWeek/main2.cpp
@@ -13,6 +13,9 @@
 #include <thread>
 #include <mutex>
 
+// function
+hittable_list random_scene();
+
 // Image
 const double aspect_ratio = 16.0 / 9.0;
 const int image_width = 1600;
@@ -21,7 +24,7 @@ const int max_depth = 50;
 const int samples_per_pixel = 60;
 
 // World
-hittable_list world;
+//hittable_list world;
 
 // Camera old before chapter 11
 //camera cam(90.0, aspect_ratio);
@@ -32,11 +35,24 @@ hittable_list world;
 //camera cam(point3(-2,2,1), point3(0,0,-1), vec3(0,1,0), 20, aspect_ratio);
 
 // Camera new after chapter 12
-point3 lookfrom(3,3,2);
-point3 lookat(0,0,-1);
-vec3 vup(0,1,0);
-auto dist_to_focus = (lookfrom-lookat).length();
-auto aperture = 2.0;
+//point3 lookfrom(3, 3, 2);
+//point3 lookat(0, 0, -1);
+//vec3 vup(0, 1, 0);
+//auto dist_to_focus = (lookfrom - lookat).length();
+//auto aperture = 2.0;
+//camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
+
+// World
+hittable_list world = random_scene();
+
+// Camera
+point3 lookfrom(13, 2, 3);
+point3 lookat(0, 0, 0);
+vec3 vup(0, 1, 0);
+auto dist_to_focus = 10.0;
+auto aperture = 0.1;
+
 camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
 std::vector<std::vector<color>> color_table(image_height + 1, std::vector<color>(image_width + 1));
@@ -79,21 +95,53 @@ void scan_calculate_color(int height, int width) {
     write_color_table(pixel_color, samples_per_pixel, color_table, j, i);
 }
 
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 int main() {
-
-    auto R = cos(pi / 4);
-
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
-    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
